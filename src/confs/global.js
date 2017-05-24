cbf21833db3286e5ae99a892b41a86c3acfceb9a
Eray Arslan <relfishere@gmail.com>
2017-05-24 13:20:21 +0300

    confs updated

--- src/confs/global.js
+++ src/confs/global.js
@@ -4,13 +4,7 @@ module.exports = {
   jokerToken: "BLYAD",
   requestTimeout: 5 * 1000,
   db: $env_config['mongo_uri'],
-  mail: {
-    user: "",
-    smtp: "",
-    password: "",
-    from: "bot@sausozluk.net",
-    name: "saü sözlük bot"
-  },
+  mail: $env_config['mail'],
   randomCount: 5,
   ids: ['entries_inc', 'topics_inc']
 };
