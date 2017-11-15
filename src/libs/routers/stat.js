4e5027e4554e178f64dab855a0678813e7ef2d08
Eray Arslan <eray.arslan@hepsiburada.com>
2017-11-15 08:57:14 +0300

    general security policy updated

--- src/libs/routers/stat.js
+++ src/libs/routers/stat.js
@@ -2,5 +2,5 @@ module.exports = function (app) {
   app.get("/stats/most-writers", secure, routers["stat"].mostWriterUsers);
   app.get("/stats/most-up-voted", secure, routers["stat"].mostUpVotedEntries);
   app.get("/stats/new-users", secure, routers["stat"].newUsers);
-  app.get("/stats/general", routers["stat"].general);
+  app.get("/stats/general", secure, routers["stat"].general);
 };
